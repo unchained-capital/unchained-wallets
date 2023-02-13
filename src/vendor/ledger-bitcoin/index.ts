13c973996ddc2cd8f07834747511a80ed84ce5b2
Buck Perley <buck@unchained-capital.com>
2023-02-13 17:02:15 -0600

    fix: accidentally git ignoring ledger-bitcoin files

--- src/vendor/ledger-bitcoin/index.ts
+++ src/vendor/ledger-bitcoin/index.ts
@@ -1,6 +1,6 @@
-import AppClient from './lib/appClient';
-import { DefaultWalletPolicy, WalletPolicy } from './lib/policy';
-import { PsbtV2 } from './lib/psbtv2';
+import AppClient from "./src/appClient";
+import { DefaultWalletPolicy, WalletPolicy } from "./src/policy";
+import { PsbtV2 } from "./src/psbtv2";
 
 export { AppClient, PsbtV2, DefaultWalletPolicy, WalletPolicy };
 
